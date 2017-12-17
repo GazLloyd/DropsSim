76a35f9b936b44e9175c7e68b4e363fa8be58949
Gaz L <gazllloyd@gmail.com>
2017-12-17 17:18:29 +0000

    objectify, expand, and fix issues

--- src/AscensionBows.java
+++ src/AscensionBows.java
@@ -4,45 +4,98 @@ import java.util.Random;
  * Created by gaz-l on 17/12/2017.
  */
 public class AscensionBows {
-    public static long simBows(int bowgoal) {
-        long kills = 0, bows = 0;
 
-        int[] signets = {0,0,0,0,0,0};
+    private int[] signets = {0,0,0,0,0,0};
+    private Random r = new Random();
+    public int creatureKills = 0, legioKills = 0, goal;
+    public int[] legioKillsEach = {0,0,0,0,0,0}, extraKeys = {0,0,0,0,0,0};
 
-        Random r = new Random();
 
-        while (bows < bowgoal) {
-            kills++;
+    public AscensionBows(int num) {
+        goal = num;
+        sim();
+    }
+
+    private boolean checkSignet(int k) {
+        return signets[k] >= goal;
+    }
+
+    private boolean checkSignets() {
+        return signets[0] >= goal &&
+                signets[1] >= goal &&
+                signets[2] >= goal &&
+                signets[3] >= goal &&
+                signets[4] >= goal &&
+                signets[5] >= goal;
+    }
+
+
+    private void keyDrop(int k) {
+        if (checkSignet(k)) {
+            extraKeys[k]++;
+        } else {
+            killLegio(k);
+        }
+    }
+
+    private void killLegio(int k) {
+        legioKills++;
+        legioKillsEach[k]++;
+        // 1/50 chance of a signet
+        if (r.nextInt(50) == 0) {
+            signets[k]++;
+        }
+        // 1/64 chance of another key (can get both signet and key)
+        if (r.nextInt(64) == 0) {
+            keyDrop(r.nextInt(6));
+        }
+    }
+
+
+    private void sim() {
+        while (!checkSignets()) {
+            creatureKills++;
+            // 1/64 chance of a key
             if (r.nextInt(64) == 0) {
-                int k = r.nextInt(6);
-                if (r.nextInt(50) == 0) {
-                    signets[k]++;
-                }
-            }
-
-            if (signets[0] > 0 && signets[1] > 0 && signets[2] > 0 && signets[3] > 0 && signets[4] > 0 && signets[5] > 0) {
-                bows++;
-                signets[0]--;
-                signets[1]--;
-                signets[2]--;
-                signets[3]--;
-                signets[4]--;
-                signets[5]--;
+                //each key equally likely (1/6)
+                keyDrop(r.nextInt(6));
             }
         }
-
-        return kills;
     }
 
     public static void main(String... args) {
-        int sample = 100000, bows = 2;
-        long totalkills = 0;
+        int sample = 100000, bows = 1;
+        double totalkills = 0, totallegkills = 0;
+        double[] totallegkillseach = {0,0,0,0,0,0}, totalextrakeys = {0,0,0,0,0,0};
+
         for (int i = 0; i < sample; i++) {
-            totalkills += simBows(bows);
+            AscensionBows ab = new AscensionBows(bows);
+            totalkills += ab.creatureKills;
+            totallegkills += ab.legioKills;
+            for (int j = 0; j < 6; j++) {
+                totallegkillseach[j] += ab.legioKillsEach[j];
+                totalextrakeys[j] += ab.extraKeys[j];
+            }
         }
 
         System.out.println("Bows to get: " + bows);
         System.out.println("Sample size: " + sample);
         System.out.println("Average Ascension creature kills: " + totalkills/sample);
+        System.out.println("Average Legiones kills (total): " + totallegkills/sample);
+        System.out.println();
+        System.out.println("Average Primus kills: " + totallegkillseach[0]/sample);
+        System.out.println("Average Secundus kills: " + totallegkillseach[1]/sample);
+        System.out.println("Average Tertius kills: " + totallegkillseach[2]/sample);
+        System.out.println("Average Quartus kills: " + totallegkillseach[3]/sample);
+        System.out.println("Average Quintus kills: " + totallegkillseach[4]/sample);
+        System.out.println("Average Sextus kills: " + totallegkillseach[5]/sample);
+        System.out.println();
+        System.out.println("Average extra keys: " + (totalextrakeys[0]+totalextrakeys[1]+totalextrakeys[2]+totalextrakeys[3]+totalextrakeys[4]+totalextrakeys[5])/sample);
+        System.out.println("Average extra Primus keys: " + totalextrakeys[0]/sample);
+        System.out.println("Average extra Secundus keys: " + totalextrakeys[1]/sample);
+        System.out.println("Average extra Tertius keys: " + totalextrakeys[2]/sample);
+        System.out.println("Average extra Quartus keys: " + totalextrakeys[3]/sample);
+        System.out.println("Average extra Quintus keys: " + totalextrakeys[4]/sample);
+        System.out.println("Average extra Sextus keys: " + totalextrakeys[5]/sample);
     }
 }
--- src/Khopesh.java
+++ src/Khopesh.java
@@ -4,48 +4,74 @@ import java.util.Random;
  * Created by gaz-l on 17/12/2017.
  */
 public class Khopesh {
-    public static long sim(int goal) {
-        long kills = 0, khops = 0, scraps = 0;
 
-        Random r = new Random();
+    private int scraps = 0, phylChance = 18;
+    Random r;
+
+    public int creatureKills = 0, magisterKills = 0;
+
+
+    public Khopesh(int num, boolean lotd) {
+        if (lotd)
+            phylChance = 17;
+        sim(num);
+    }
+
+    private void killMagister() {
+        magisterKills++;
+        // 1/18 chance of a phyl (1/17 lotd)
+        if (r.nextInt(phylChance) == 0) {
+            // phyl distribution: 40/100 of 5 scraps, 30/100 of 10, 25/100 15, 5/100 of 25
+            int scrapN = r.nextInt(100);
+            if (scrapN < 40) {
+                scraps += 5;
+            } else if (scrapN < 70) {
+                scraps += 10;
+            } else if (scrapN < 95) {
+                scraps += 15;
+            } else {
+                scraps += 25;
+            }
+        }
+        // key dropped, do another kill right away (with no extra
+        if (r.nextInt(128) == 0) {
+            killMagister();
+        }
+    }
+
+    private void sim(int goal) {
+        long khops = 0;
+        scraps = 0;
+        r = new Random();
 
         while (khops < goal) {
-            kills++;
+            creatureKills++;
             // 1/128 chance of a key
             if (r.nextInt(128) == 0) {
-                // 1/18 chance of a phyl
-                if (r.nextInt(18) == 0) {
-                    // phyl distribution: 40/100 of 5 scraps, 30/100 of 10, 25/100 15, 5/100 of 25
-                    int scrapN = r.nextInt(100);
-                    if (scrapN < 40) {
-                        scraps += 5;
-                    } else if (scrapN < 70) {
-                        scraps += 10;
-                    } else if (scrapN < 95) {
-                        scraps += 15;
-                    } else {
-                        scraps += 25;
-                    }
-                }
+                // perform a magister kill
+                killMagister();
             }
             if (scraps >= 300) {
                 scraps -= 300;
                 khops++;
             }
         }
-
-        return kills;
     }
 
     public static void main(String... args) {
         int sample = 100000, khopeshes = 1;
-        long totalkills = 0;
+        boolean lotd = true;
+        long totalkills = 0, totalmagkills = 0;
         for (int i = 0; i < sample; i++) {
-            totalkills += sim(khopeshes);
+            Khopesh k = new Khopesh(khopeshes, lotd);
+            totalkills += k.creatureKills;
+            totalmagkills += k.magisterKills;
         }
 
         System.out.println("Khopeshes to get: " + khopeshes);
+        System.out.println("LotD?: "+lotd);
         System.out.println("Sample size: " + sample);
         System.out.println("Average corrupted creature/soul devourer kills: " + totalkills / sample);
+        System.out.println("Average Magister kills: " + totalmagkills / sample);
     }
 }
